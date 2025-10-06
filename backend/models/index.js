cbb0b41b28933f94a086b5f3713d813215bb7311
Mahabubur Shiam <mahabubur.rahman@g.bracu.ac.bd>
2025-10-06 16:30:42 +0600

    Gitignore added

--- backend/models/index.js
+++ backend/models/index.js
@@ -4,4 +4,16 @@ const User= require('./users.js');
 
 const WorkoutGoal= require('./workoutgoals.js');
 
-const DailyLog = require 
+const DailyLog = require('./dailylogs.js');
+
+const SleepLog = require('./sleeplogs.js');
+
+const WorkoutSession = require('./workoutSessions.js');
+
+
+//const Journal=require('./journals.js); // will fetch API data
+
+//Avaiable Gyms
+
+//Wearable Devices
+//Nutrition Plans
